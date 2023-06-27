b90dfad2f16d9dfaebd434f31e864cf24cd2fa30
CaptainPMP <scikung8@gmail.com>
2023-06-28 01:25:32 +0700

    success Markdown Editor by ReactJS tutorial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import './main.scss'
+import MarkdownEditor from './components/MarkdownEditor'
+
+function App() {
+  return (
+    <>
+      <MarkdownEditor />
+    </>
+  )
+}
+
+export default App
